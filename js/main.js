fe5d771428f39ed2127313a40a7bcbefb92d9f6a
daySosa <daym_sosan50@unicah.edu>
2026-08-04 10:08:24 -0600

    Home Page

--- js/main.js
+++ js/main.js
@@ -205,7 +205,6 @@ function updateCartBadge() {
 
 
 
-
 function initSearch() {
   var trigger = document.getElementById("searchTrigger");
   var overlay = document.getElementById("searchOverlay");
